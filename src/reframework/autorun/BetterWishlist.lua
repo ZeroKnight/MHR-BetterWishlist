cba485c189a9d9e0369e332f73e1709f94545c4e
ZeroKnight <xzeroknightx@gmail.com>
2022-12-05 01:59:19 -0800

    feat: Improve filtering even further
    The mod now has even smarter filtering compared to the naive initial
    implementation; you will *only* ever be shown wishlist items that you
    can actually craft, whether it has category materials or not. Even
    better, it will only show the "required materials gathered" message and
    omit the now redundant "material types" message (if applicable).
    
    The initial implementation was flawed such that if you had met the
    primary materials requirement for an item, but lacked the category
    materials, you would still get a notification. While this isn't likely
    to happen for weapons, it could be quite likely to happen for layered
    armor/weapons.
    
    I was able to figure out enough of how WishList items are handled to do
    the necessary cross-checking and rebuild the list of items sent to chat.
    The wishlist now behaves much more intuitively, huzzah!

--- src/reframework/autorun/BetterWishlist.lua
+++ src/reframework/autorun/BetterWishlist.lua
@@ -4,10 +4,68 @@
 -- Having enough of such parts for a wishlisted recipe is what triggers the
 -- "Required material **types** gathered" message.
 
+local WishListManager = sdk.get_managed_singleton 'snow.data.WishListManager'
 local WishListManagerType = sdk.find_type_definition 'snow.data.WishListManager'
+local tmo = sdk.to_managed_object
+local fmt = string.format
 
--- Skipping this method only prevents the "material type gathereds" message,
--- but not the "materials gathered" message, which is what we want.
-sdk.hook(WishListManagerType:get_method 'checkWishListCategoryCompleteVillage', function(_)
-  return sdk.PreHookResult.SKIP_ORIGINAL
-end, function(_) end)
+local function get_WishInfoList()
+  return tmo(tmo(WishListManager:get_field '_WishInfoListData'):get_field '_WishInfoList')
+end
+
+local function get_LogWishList()
+  return tmo(tmo(WishListManager:get_field '_LogWishList'):get_field '_DispLogWishList')
+end
+
+local function post_initializeBeforeVillage(_)
+  local seen = {}
+  local has_category = {}
+  local complete_wishlist_items = {}
+  local wishinfolist = get_WishInfoList()
+  local logwishlist = get_LogWishList()
+
+  -- Determine which wishlist entries have category materials
+  for i = 0, wishinfolist:get_Count() - 1 do
+    local id = wishinfolist[i]:get_DataId()
+    if id ~= 0 then
+      -- snow.data.NormalItemData.MaterialCategory.None = 0
+      local category = tmo(WishListManager:getWishDataCategory(i)):get_field '_Category'
+      has_category[id] = category ~= 0
+    end
+  end
+
+  -- Collect truly complete wishlist items, i.e. primary *and* possible category materials
+  for i = 0, logwishlist:get_Count() - 1 do
+    local wish = logwishlist[i]
+    local id = wish:get_DataId()
+    local isCategory = wish:isCategory()
+    local wishdata = { wish = wish, idx = i }
+
+    if not has_category[id] then
+      -- Always add completed items with no category materials
+      log.debug(fmt('Adding item with no category materials: %d', id))
+      table.insert(complete_wishlist_items, wishdata)
+    elseif seen[id] == nil then
+      -- Potential completed item
+      log.debug(fmt('Seen item %d (Category: %s)', id, tostring(isCategory)))
+      seen[id] = wishdata
+    else
+      -- Found corresponding entry, add non-category half to the complete list
+      log.debug(fmt('Found match for %d (Category: %s)', id, tostring(isCategory)))
+      if isCategory then
+        table.insert(complete_wishlist_items, seen[id])
+      else
+        table.insert(complete_wishlist_items, wishdata)
+      end
+    end
+  end
+  log.debug(fmt('Found %d complete wishlist items', #complete_wishlist_items))
+
+  -- Rebuild LogWishList with only truly complete items
+  logwishlist:Clear()
+  for _, item in ipairs(complete_wishlist_items) do
+    logwishlist:Add(item.wish)
+  end
+end
+
+sdk.hook(WishListManagerType:get_method 'initializeBeforeVillage', function(_) end, post_initializeBeforeVillage)
